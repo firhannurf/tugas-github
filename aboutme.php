865c46bae62ced75aec5f79ac42e0b79da535be8
firhannurf <63441025+firhannurf@users.noreply.github.com>
2020-04-10 13:05:04 +0700

    Create aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,10 @@
+<?php
+echo"Hello world";
+echo"| My name is Firhan Nur F";
+echo"| i'm from XI RPL 2";
+echo"| my address in jl h soleh gg jambu no 32 code 16455";
+echo"| my phone number is 08978113075";
+echo"| if you want to message me you can call me at that number";
+echo"| firhannurf@gmail.com|";
+echo"| that my email if you want to send me a message in my email, i put my email on this site |";
+?>
